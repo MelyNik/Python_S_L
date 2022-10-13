ae95de817678661320f1c2ca422db9dc01feca23
Nikita <number_nik@mail.ru>
2022-10-13 19:09:56 +0300

    корректировка

--- D_Z/Seminar_5/1_2.py
+++ D_Z/Seminar_5/1_2.py
@@ -9,38 +9,42 @@
 # 'text_code_words.txt'
 # Enter the name of the file to decode:
 # 'text_code_words.txt'
-# 
+#
 # out
 # aaaaavvvvvvvvvvvvvvvvvvvvvvvvvvvvvssssDDDdddFFggggOOiiiaa
 # vvvvvvvvvvvbbwwPPuuuTTYyWWQQ
-# 
+#
 # out in file
 # 'text_words.txt'
 # aaaaavvvvvvvvvvvvvvvvvvvvvvvvvvvvvssssDDDdddFFggggOOiiiaa
 # vbbwwPPuuuTTYyWWQQ
-# 
+#
 # 'text_code_words.txt'
 # 5a29v4s3D3d2F4g2O3i2a1
 # 1v2b2w2P3u2T1Y1y2W2Q
 
 
-# Самостоятельно не справился, только разбрав уже готовое.
+# Самостоятельно не справился, только разобрав уже готовое.
 
 
 from itertools import groupby, starmap
-from os import path 
+from os import path
 
-def Encode(text='text.txt' , final_text='text_f.txt'):
-    if path.exists(text) and not path.exists(final_text): # Проверяем наличие пути только файла text.
-        # path.exists - йункция которая проверяет наличие пути у файла или его отсутствие.
+
+def Encode(text='text.txt', final_text='text_f.txt'):
+    # Проверяем наличие пути только файла text.
+    if path.exists(text) and not path.exists(final_text):
+        # path.exists - функция которая проверяет наличие пути у файла или его отсутствие.
         with open(text) as f_1, \
-            open(final_text, 'a') as f_2:
+                open(final_text, 'a') as f_2:
             for i in f_1:
-                f_2.write(''.join([f'{len(list(l))}{letter}' for letter, l in groupby(i)]))
-                # Пишем в файл 2 функцией  groupby(i) где i это строка, letter это буква, а l это количество 
+                f_2.write(
+                    ''.join([f'{len(list(l))}{letter}' for letter, l in groupby(i)]))
+                # Пишем в файл 2 функцией  groupby(i) где i это строка, letter это буква, а l это количество
                 # таких же букв идущих подряд, и получилась длина списка из этих букв {len(list(l))}{letter}.
     else:
         print('The files do not exists in the system!')
+
 
 def Decode(result):
     if path.exists(result):
@@ -58,5 +62,7 @@ def Decode(result):
     else:
         print('The files do not exists in the system!')
 
-Encode(input('Enter the name of the file with the text\n'), input('Enter the file name to record\n'))
+
+Encode(input('Enter the name of the file with the text\n'),
+       input('Enter the file name to record\n'))
 Decode(input('Enter the name of the file to decode\n'))
